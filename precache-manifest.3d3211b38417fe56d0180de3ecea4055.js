023925201eee614bb2f0b9df1c06953e96011056
hcaula <lhtc@cin.ufpe.br>
2020-04-02 00:30:17 -0300

    Updates

---
+++ precache-manifest.3d3211b38417fe56d0180de3ecea4055.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1912a525a7e8b7bf6316fa46575c2f0b",
+    "url": "/hihi/index.html"
+  },
+  {
+    "revision": "70a5a963b93638230a23",
+    "url": "/hihi/static/css/main.0ee75a2a.chunk.css"
+  },
+  {
+    "revision": "2f967e5648fb3d99c51d",
+    "url": "/hihi/static/js/2.3525f8b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hihi/static/js/2.3525f8b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70a5a963b93638230a23",
+    "url": "/hihi/static/js/main.e0d84fc3.chunk.js"
+  },
+  {
+    "revision": "6adc7fda82129ff0a91f",
+    "url": "/hihi/static/js/runtime-main.54ebcd9a.js"
+  },
+  {
+    "revision": "99b983d92c03c7d59a43304472629869",
+    "url": "/hihi/static/media/2018-11-17.99b983d9.jpg"
+  },
+  {
+    "revision": "a5bcd62c3a14b348e2081be813b07a8b",
+    "url": "/hihi/static/media/2018-11-20.a5bcd62c.jpg"
+  },
+  {
+    "revision": "f2315be0586ba4bae1605ea9e78eb222",
+    "url": "/hihi/static/media/2018-11-25.f2315be0.jpg"
+  },
+  {
+    "revision": "35b4c8a187beb701061e1d37ae1083b1",
+    "url": "/hihi/static/media/2018-12-17.35b4c8a1.jpg"
+  },
+  {
+    "revision": "32e0721080b1554b368514eb25f67e50",
+    "url": "/hihi/static/media/2018-12-21.32e07210.jpg"
+  },
+  {
+    "revision": "0dfa694959c46e45c60821a574fc2ee6",
+    "url": "/hihi/static/media/2019-01-01.0dfa6949.jpg"
+  },
+  {
+    "revision": "d3e86f664a6b9273f9a755375638de1b",
+    "url": "/hihi/static/media/2019-03-16.d3e86f66.jpg"
+  },
+  {
+    "revision": "c2d487f4b9facde2e75bb25d8887e97d",
+    "url": "/hihi/static/media/2019-04-27.c2d487f4.jpg"
+  },
+  {
+    "revision": "2fc78af0c6f14b35a6382acfb8a23919",
+    "url": "/hihi/static/media/2019-05-19.2fc78af0.jpg"
+  },
+  {
+    "revision": "af463da1fc01873c2f1c23403f43e90b",
+    "url": "/hihi/static/media/2019-08-09.af463da1.jpg"
+  },
+  {
+    "revision": "d882f00ca433fa0a786837c5cefe3ba7",
+    "url": "/hihi/static/media/2019-11-16.d882f00c.jpg"
+  },
+  {
+    "revision": "83f00e1ae63ee37b1c6519c2a9ba327a",
+    "url": "/hihi/static/media/2019-12-22.83f00e1a.jpg"
+  },
+  {
+    "revision": "0355cc95fc7966b1f1b22563a811890c",
+    "url": "/hihi/static/media/2020-01-18.0355cc95.jpg"
+  },
+  {
+    "revision": "1a00134f7baf6a4783dc1135145a361b",
+    "url": "/hihi/static/media/Roboto-Black.1a00134f.ttf"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/hihi/static/media/Roboto-Light.d5c46b13.ttf"
+  }
+]);
